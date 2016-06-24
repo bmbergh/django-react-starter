199c48ada2d6a7c9048ea937b5e0ca3fa54e0e01
Brandy Bergh <brandy@prpl.rs>
2016-06-23 21:46:45 -0400

    intial webpack file setup

---
+++ settings.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'), # We do this so that django's collectstatic copies or our bundles to the STATIC_ROOT or syncs them to whatever storage we use.
+)
+
+WEBPACK_LOADER = {
+    'BUNDLE_DIR_NAME': 'bundles/',
+    'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+}
+
+INSTALLED_APPS = (
+ ...
+ 'webpack_loader',
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,3 +2,43 @@ var path = require('path');
 var webpack = require('webpack');
 var tracker = require('webpack-bundle-tracker');
 
+module.exports = {
+  context: __dirname,
+
+  entry: './assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+  output: {
+      path: path.resolve('./assets/bundles/'),
+      filename: "bundle.[name].js",
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
+  module: {
+    loaders: [
+      { //npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015','react']
+        }
+      },
+      { //npm install --save-dev style-loader css-loader
+        test: /\.css$/,
+        loader: 'style!css?modules!postcss'
+      },
+      {
+        test:/\.json$/,
+        loader:'json'
+      }
+    ],
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+    extensions: ['', '.js', '.jsx']
+  },
+}
